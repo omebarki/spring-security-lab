111f791d6df6368f971ee52f4ab0b7e61cedcc3a
Omar MEBARKI <omar.mebarki@gmail.com>
2021-05-30 20:17:08 +0200

    Do not use session to store the Authentication

--- src/main/java/omar/mebarki/security/config/SecurityConfig.java
+++ src/main/java/omar/mebarki/security/config/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration()
@@ -30,5 +31,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().anyRequest().authenticated();
         http.addFilterBefore(new AuthenticationRequestFilter(),BasicAuthenticationFilter.class);
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 }
