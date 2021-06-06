256b5a404784aa82a7ec994385d1d98bbb341273
Omar MEBARKI <omar.mebarki@gmail.com>
2021-06-06 23:00:29 +0200

    Using AuthenticationFilter

--- src/main/java/omar/mebarki/security/config/SecurityConfig.java
+++ src/main/java/omar/mebarki/security/config/SecurityConfig.java
@@ -1,17 +1,29 @@
 package omar.mebarki.security.config;
 
 import omar.mebarki.security.auth.CustomAuthenticationProvider;
-import omar.mebarki.security.auth.OmarAuthenticationRequestFilter;
 import omar.mebarki.security.rest.HelloRestController;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationFilter;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Configuration()
 @EnableWebSecurity
@@ -21,6 +33,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private CustomAuthenticationProvider authProvider;
 
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.authenticationProvider(authProvider);
@@ -29,7 +42,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().anyRequest().authenticated();
-        http.addFilterBefore(new OmarAuthenticationRequestFilter(),BasicAuthenticationFilter.class);
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilterBefore(getAuthenticationFiler(), BasicAuthenticationFilter.class);
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.ALWAYS);
+    }
+
+    public AuthenticationFilter getAuthenticationFiler() throws Exception{
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter(authenticationManager(), request -> {
+            return new UsernamePasswordAuthenticationToken(request.getParameter("login"), request.getParameter("password"));
+        });
+        AuthenticationSuccessHandler successHandler =(HttpServletRequest request, HttpServletResponse response, Authentication authentication) -> { };
+        authenticationFilter.setSuccessHandler(successHandler);
+        return authenticationFilter;
     }
 }
